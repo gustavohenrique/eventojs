d34f87eabbdf5501f12d5aec4771bf0cd2ff2598
Gustavo Henrique <gustavo@gustavohenrique.net>
2014-10-27 18:38:47 -0200

    Added more tests

--- easyevent.js
+++ easyevent.js
@@ -2,20 +2,47 @@ var EasyEvent = (function () {
     
     var self = this;
 
-    function _bind (evt, fn) {
-        self._events = self._events || {};
-        self._events[evt] = fn;
+    var commonFunctions = {
+        bind: function (evt, fn) {
+            self._events = self._events || {};
+            self._events[evt] = fn;
+        },
+
+        trigger: function (evt) {
+            self._events = self._events || {};
+            if (! self._events.hasOwnProperty(evt)) return;
+            self._events[evt].apply();
+        }
     };
 
-    function _trigger (evt) {
-        self._events = self._events || {};
-        if (! self._events.hasOwnProperty(evt)) return;
-        self._events[evt].apply();
+    var visualFunctions = {
+        addClass: function (className) {},
+        removeClass: function (className) {},
+        show: function (className) {},
+        hide: function (className) {},
+        addEvent: function (className) {},
+        removeEvent: function (className) {}
+    };
+
+    var _add = function (func, functions, obj) {
+        if (typeof obj === 'function') {
+            obj.prototype[func] = functions[func];
+        }
+        else {
+            obj[func] = functions[func];
+        }
     };
 
     self.applyTo = function (obj) {
-        obj.bind = _bind;
-        obj.trigger = _trigger;
+        for (func in commonFunctions) {
+            _add(func, commonFunctions, obj);
+        }
+
+        for (func in visualFunctions) {
+            if (obj.hasOwnProperty('isHtml') && obj.isHtml) {
+                _add(func, visualFunctions, obj);
+            }
+        }
     };
 
     return self;
